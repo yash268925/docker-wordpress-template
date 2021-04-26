8704cd9d5db7980b2cbf181e5d41357421ed34e3
yash268925 <yash.2689.25@gmail.com>
2021-04-26 15:56:26 +0900

    間違い修正

--- cli-scripts/install.sh
+++ cli-scripts/install.sh
@@ -6,4 +6,4 @@ wp core install \
   --admin_user="$WORDPRESS_ADMIN_USER" \
   --admin_password="$WORDPRESS_ADMIN_PASSWORD" \
   --admin_email="$WORDPRESS_ADMIN_EMAIL" \
-  $( [[ ! $WORDPRESS_ADMIN_EMAIL_SKIP ]] && echo '--skip-email' )
+  $( [[ $WORDPRESS_ADMIN_EMAIL_SKIP ]] && echo '--skip-email' )
